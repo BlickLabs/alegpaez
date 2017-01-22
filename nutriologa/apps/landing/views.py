1190c73920691c3ade8344706a1c8fdc25db6fad
Mauricio Ivan Mejía Ramirez <mauriciodinki@gmail.com>
2017-01-22 14:58:41 -0600

    Add images to post info

--- nutriologa/apps/landing/views.py
+++ nutriologa/apps/landing/views.py
@@ -2,7 +2,13 @@
 # -*- coding: utf-8 -*
 from django.views.generic import TemplateView
 
-from nutriologa.apps.wordpress.models import WpPosts
+from nutriologa.apps.wordpress.models import WpPosts, WpPostmeta
+
+
+class Thumbnail():
+    def __init__(self, post_id, url):
+        self.post_id = post_id
+        self.url = url
 
 
 class HomeTemplateView(TemplateView):
@@ -10,6 +16,16 @@ class HomeTemplateView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomeTemplateView, self).get_context_data(**kwargs)
+        thumbnails = []
         posts = WpPosts.objects.filter(post_status='publish')[:5]
+        metas = WpPostmeta.objects.all()
+        for post in posts:
+            for meta in metas:
+                if meta.post_id == post.id and meta.meta_key == '_thumbnail_id':
+                    thumbnail_id = meta.meta_value
+                    thumb = WpPostmeta.objects.get(post_id=thumbnail_id, meta_key='_wp_attached_file')
+                    thumbnails.append(Thumbnail(post.id, thumb.meta_value))
         context['posts'] = posts
+        context['thumbnails'] = thumbnails
+        context['metas'] = metas
         return context
